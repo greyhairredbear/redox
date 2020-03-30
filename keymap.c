9591e541b8646dfbcebc1f6ce6550203e1ec1f2a
Peter Laggner <peter.laggner@bytepoets.com>
2020-03-30 11:37:52 +0200

    exchange del and bspc on left half

--- keymap.c
+++ keymap.c
@@ -13,11 +13,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,  KC_EQL,                           KC_BSLS,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-   LCTL(KC_1),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,  KC_DEL,                           KC_BSPC,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT, 
+   LCTL(KC_1),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G, KC_BSPC,                           KC_BSPC,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT, 
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
   LALT(KC_F7),    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   MO(1), KC_LGUI,           TG(2), KC_RALT,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RBRC,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-LCTL(KC_SLSH),LSFT(KC_F6),KC_BSPC, KC_GRV,     KC_LCTL,     KC_LSFT, KC_LALT,           MO(1),  KC_ENT,      KC_SPC,      KC_LEFT,   KC_UP, KC_DOWN, KC_RGHT
+LCTL(KC_SLSH),LSFT(KC_F6),KC_DEL, KC_GRV,      KC_LCTL,     KC_LSFT, KC_LALT,           MO(1),  KC_ENT,      KC_SPC,      KC_LEFT,   KC_UP, KC_DOWN, KC_RGHT
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   ),
 
