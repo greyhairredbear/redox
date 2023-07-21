062c43aaf4624c14721905183c47df6c48f58a60
Peter Laggner <peter.laggner@cargonexx.de>
2023-07-21 11:46:30 +0200

    use tapping term in config

--- config.h
+++ config.h
@@ -26,6 +26,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
+#define TAPPING_TERM 100
+
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 14
--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,2 @@
 cp -f keymap.c $QMK_HOME/keyboards/redox/keymaps/plaggner
+cp -f config.h $QMK_HOME/keyboards/redox/keymaps/plaggner
