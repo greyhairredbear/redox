55b9106a6dd987eb4ac21eb4eaf9f09c76e66251
Peter Laggner <peter.laggner@cargonexx.de>
2023-07-25 10:22:06 +0200

    adjust tapping term duration

--- config.h
+++ config.h
@@ -26,7 +26,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-#define TAPPING_TERM 100
+#define TAPPING_TERM 150
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
